8b182c294fad4cb9ebceede10704b125a0a26c85
saalxx <saalxx@alumnatflorida.es>
2026-02-18 12:45:18 +0100

    Subido carrusel terminado y mejorado junto a las imagenes su calidad para su funcionalidad

--- frontend/src/app/views/index/index.ts
+++ frontend/src/app/views/index/index.ts
@@ -1,100 +1,77 @@
-import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { NgStyle } from '@angular/common';
-import { Carrusel } from '../../components/carrusel/carrusel';
 import { RouterLink } from '@angular/router';
+import { Carrusel } from '../../components/carrusel/carrusel';
 
 @Component({
   selector: 'app-index',
+  standalone: true,
   imports: [NgStyle, Carrusel, RouterLink],
   templateUrl: './index.html',
   styleUrl: './index.css',
 })
-export class Index {
-  images = [
-    'https://ocioenvalencia.es/wp-content/uploads/2025/01/convento.jpg',
-    'https://www.hellovalencia.es/wp-content/uploads/2019/07/Discoteca_Valencia_Marina-Beach-Club.jpg',
-    'https://offloadmedia.feverup.com/valenciasecreta.com/wp-content/uploads/2022/10/13124225/sala-cine-mas-visitada-valencia.jpg',
-    'https://bergamonte.es/wp-content/uploads/2022/08/clases-de-padel-Valencia-Deporte-scaled.jpg',
-    'https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1536,h_2048/https://unicovalencia.com/wp-content/uploads/2025/09/2-3b7fca34-1536x2048.jpg',
-    'https://img.freepik.com/fotos-premium/vibrante-fiesta-playa-al-atardecer-dj-invitados-bailar_252600-24073.jpg?semt=ais_hybrid&w=740&q=80',
-    'https://www.visita-valencia.com/wp-content/uploads/2021/05/imagenes-almuerzos-valencia_0003_almuerzo-bar-mistela.jpg',
+export class Index implements OnInit, OnDestroy {
+
+  images: string[] = [
+    'imgs/eventosimgs/tardeo.jpg',
+    'imgs/eventosimgs/almuerzo.jpg',
+    'imgs/eventosimgs/cenar.jpg',
+    'imgs/eventosimgs/centro_comercial.jpg',
+    'imgs/eventosimgs/cine.jpg',
+    'imgs/eventosimgs/tomar_algo.jpg',
+    'imgs/eventosimgs/comida.jpg',
+    'imgs/eventosimgs/cumpleanos.jpg',
+    'imgs/eventosimgs/deporte.jpg',
   ];
-
-  private cdr = inject(ChangeDetectorRef);
-  currentImage = this.images[0];
+  currentImage!: string;
+  nextImage!: string;
   index = 0;
-  
-  public ngOnInit(): void {
-    setInterval(() => {
-      this.index = (this.index + 1) % this.images.length;
-      this.currentImage = this.images[this.index];
-      this.cdr.markForCheck();
-    }, 3000);
-  }
-}
-  
+  transitioning = false;
 
-
-  
-
-
-
-
-/*
+  private intervalId!: number;
   private cdr = inject(ChangeDetectorRef);
-  private service = inject(Requests);
 
-  public test: boolean = true;
-  public char: boolean = true;
-  public prev: string = '';
-  public next: string = '';
-  public name: string = '';
-  public image: string = '';
-  public characters: Character[] = [];
-  public current: number = 0;
-  public index: number = 0;
-
-  public onCarousel(position: number): void {
-    this.current += position;
-    if (this.char === true) {
-      if (this.current === this.characters.length) this.current = 0;
-      if (this.current < 0) this.current = this.characters.length - 1;
-      this.image = this.characters[this.current].image;
-      this.name = this.characters[this.current].name;
-    } 
+  async ngOnInit(): Promise<void> {
+    await this.preloadImages();
+    this.currentImage = this.images[0];
+    this.intervalId = window.setInterval(() => {
+      this.changeImage();
+    }, 4000);
   }
-   public getCharacters(url: string): void {
-    this.service.getCharacter(url).subscribe((response) => {
-      this.characters = response.results;
-      this.image = this.characters[this.current].image;
-      this.name = this.characters[this.current].name;
-      if (response.info.prev !== null) {
-        this.prev = response.info.prev;
-      } else {
-        this.prev =
-          'https://rickandmortyapi.com/api/character/?page=' +
-          response.info.pages;
-      }
-      if (response.info.next !== null) {
-        this.next = response.info.next;
-      } else {
-        this.next = 'https://rickandmortyapi.com/api/character/?page=1';
-      }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+  private changeImage(): void {
+    const nextIndex = (this.index + 1) % this.images.length;
+    this.nextImage = this.images[nextIndex];
+    this.transitioning = true;
+    setTimeout(() => {
+      this.currentImage = this.nextImage;
+      this.index = nextIndex;
+      this.transitioning = false;
       this.cdr.markForCheck();
+    }, 800);
+  }
+  private preloadImages(): Promise<void> {
+    return new Promise((resolve) => {
+      let loaded = 0;
+      const total = this.images.length;
+      this.images.forEach(src => {
+        const img = new Image();
+        img.src = src;
+        img.decode?.().catch(() => {});
+        img.onload = () => {
+          loaded++;
+          if (loaded === total) resolve();
+        };
+        img.onerror = () => {
+          loaded++;
+          if (loaded === total) resolve();
+        };
+      });
     });
   }
-
-   public showMore(position: number): void {
-    this.index = 0;
-    this.current = 0;
-    if (position === -1) {
-      if (this.char === true) {
-        this.getCharacters(this.prev);
-      } 
-    } else if (position === 1) {
-      if (this.char === true) {
-        this.getCharacters(this.next);
-      }
-    }
-  }*/
-
+}
